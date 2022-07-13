0d7cbc61bfec1b9b5309d09a725204f405457797
cwyyyds <1013953614@qq.com>
2022-07-13 19:19:08 +0800

    login页面完成了一半

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <h1>黑马头条</h1>
-    <span class="toutiao toutiao-yuedu"></span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="login"></div>
+    <router-view></router-view>
   </div>
 </template>
-
+<script>
+import requset from "@/utils/request";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    console.dir(requset);
+  },
+};
+</script>
 <style lang="less">
 .login {
   width: 694px;
